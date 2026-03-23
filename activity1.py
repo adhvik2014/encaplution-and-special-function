8ed07e578836bc4239eac3c340dbfc358ff4bcca
adhvik2014 <jogimalik41@gmail.com>
2026-03-23 19:29:00 +0530

    Create activity1.py

---
+++ activity1.py
@@ -0,0 +1,8 @@
+class myclass:
+    def_privmath(self)
+print("i'm inside class myclass")
+def hello(self):
+print("private variable variable value:"myclass._privatevar)
+foo = myclass()
+foo.hello()
+foo._privmath
